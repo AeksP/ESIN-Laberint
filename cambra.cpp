5cbf16e3b6da6da336abc64b6586f52a1805b5b0
AeksP <82960421+AeksP@users.noreply.github.com>
2021-11-17 12:08:19 +0100

    Test

---
+++ cambra.cpp
@@ -0,0 +1,12 @@
+#include "cambra.hpp"
+ #include <iostream>
+ using namepsace std;
+ 
+explicit cambra::cambra(bool n=false, bool s=false, bool e=false, bool o=false){
+  cout<<"hola"<<endl;
+  n = true; s = true; e = true; o = true;
+}
+
+cambra::~cambra(){
+
+}
