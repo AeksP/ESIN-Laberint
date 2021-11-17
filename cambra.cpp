c8453051a9c8773010019bf1cd0d9137189439fb
AeksP <82960421+AeksP@users.noreply.github.com>
2021-11-17 12:31:43 +0100

    v.1

--- cambra.cpp
+++ cambra.cpp
@@ -3,10 +3,29 @@
  using namepsace std;
  
 explicit cambra::cambra(bool n=false, bool s=false, bool e=false, bool o=false){
-  cout<<"hola"<<endl;
-  n = true; s = true; e = true; o = true;
+  cout<<"test"<<endl;
+  //explicit = Si tienes cambra c2(true,true)
+  //Tendrás una cambra con n=true, s=true, e=false, o=false
+  
+  //Meter metodo privado para guardar si la puerta está abierta o no?
 }
 
 cambra::~cambra(){
 
+}
+
+bool cambra::porta_oberta(paret p) const{
+	//Usar metodos privados que guardan puerta abierta aqui? (Sería _n, _s etc)
+	if(p == paret::NORD)	return _n;
+	else if(p == paret::EST)	return _e;
+	else if(p == paret::SUD)	return _s;
+	else if(p == paret::OEST)	return _o;
+	//Poner algo por si p == paret::NO_DIR?
+}
+
+void cambra::obre_porta(paret p){
+	if(p == paret::NORD){	//Mejor esto que p == 0
+		//if(_n == 1) cout<<"La puerta ya está abierta."  //No sé si hace falta
+		_n = 1;
+	}
 }
