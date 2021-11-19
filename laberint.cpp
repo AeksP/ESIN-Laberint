2f5c6df9959b79354c507ab533885f8336cde6b7
Gx3 Studios <51827577+Gguardiola@users.noreply.github.com>
2021-11-19 20:27:17 +0100

    v1.2

---
+++ laberint.cpp
@@ -0,0 +1,49 @@
+#include "laberint.hpp"
+
+
+explicit laberint::laberint(nat num_fil=5, nat num_col=5) throw(error){
+
+}
+
+explicit laberint::laberint(std::istream & is) throw(error){
+
+}
+
+laberint::laberint(const laberint & l) throw(error){
+  
+}
+laberint& laberint::operator=(const laberint & l) throw(error){
+
+}
+laberint::~laberint() throw(){
+
+}
+
+nat laberint::num_files() const throw(){
+
+}
+nat laberint::num_columnes() const throw(){
+
+}
+
+cambra laberint::operator()(const posicio & pos) const throw(error){
+
+}
+
+void laberint::obre_porta(paret p, const posicio & pos) throw(error){
+
+
+}
+
+void laberint::tanca_porta(paret p, const posicio & pos) throw(error){
+
+}
+void laberint::print(std::ostream & os) const throw(){
+
+  
+}
+
+
+
+
+
